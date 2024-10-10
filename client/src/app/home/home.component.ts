876f741022989a1176b2fcefc4c878b2cb9413c2
Richard Gieg <richgieg@users.noreply.github.com>
2024-10-10 15:47:34 -0700

    Display products

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
+import { ProductComponent } from '../components/product/product.component';
+import { Product } from '../../types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  products: Product[] = [];
+
+  onProductOutput(product: Product) {
+    console.log(product, 'Output');
+  }
+
   ngOnInit() {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products) => {
-        console.log(products.items);
+        this.products = products.items;
       });
   }
 }
